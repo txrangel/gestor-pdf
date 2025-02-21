b62d22ec2e2edbe39d5e7586510fcc2633f03835
txrangel <joaovitorrtd@gmail.com>
2025-02-20 21:05:35 -0300

    refactor: ajustando rotas

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,6 @@ use App\Http\Controllers\UserProfileController;
 use App\Http\Middleware\CheckPermission;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-});
-
-Route::middleware('auth')->group(function () {
-    Route::prefix('user/profile')->name('user.profile.')->controller(UserProfileController::class)->group(function () {
-        Route::get('/', 'edit')->name('edit');
-        Route::patch('/', 'update')->name('update');
-        Route::delete('/', 'destroy')->name('destroy');
-    });
-});
+Route::redirect('/', '/admin');
 
 require __DIR__.'/auth.php';
